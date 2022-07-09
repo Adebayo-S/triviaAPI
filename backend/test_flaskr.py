35210e976ee01f1b9033dabdda1e1a79dc17ba95
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-09 15:42:12 +0100

    [feat]: negative testing tests added

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,9 +15,9 @@ class TriviaTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgresql://{}:{}@{}/{}".format(
-            DB_USER, DB_PASSWORD, DB_HOST, self.database_name
-        )
+            DB_USER, DB_PASSWORD, DB_HOST, self.database_name)
         setup_db(self.app, self.database_path)
+
 
         # binds the app to the current context
         with self.app.app_context():
@@ -47,6 +47,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_categories'])
         self.assertTrue(data['categories'])
 
+    def test_404_sent_request_beyond_valid_page_for_categories(self):
+        """
+        Test 404 sent request beyond valid status code for categories
+        """
+        res = self.client().get('/categories?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
     def test_get_questions(self):
         """
         Test get_questions endpoint to get the questions
@@ -59,6 +70,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(data['questions'])
         self.assertTrue(data['categories'])
+
+    def test_404_sent_request_beyond_valid_page_for_questions(self):
+        """
+        Test 404 sent request beyond valid status code
+        """
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
 
     def test_delete_question(self):
         """
@@ -78,6 +100,18 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
+    def test_404_if_question_doesnt_exist_on_delete(self):
+        """
+        Test 404 if question doesn't exist on delete
+        """
+        url = '/questions/1000'
+        res = self.client().delete(url)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
     def test_add_question(self):
         """
         Test add_question endpoint to add a question
@@ -93,7 +127,23 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
-    def test_search_questions(self):
+    def test_400_if_question_added_is_invalid(self):
+        """
+        Test 400 if question added is invalid
+        """
+        res = self.client().post('/questions', json={
+            'question': 'when did Nigeria gain her independence',
+            'answer': '1960',
+            'category': [],
+            'difficulty': '1'
+        })
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'bad request')
+
+    def test_search_questions_with_results(self):
         """
         Test search_questions endpoint to return results
         for search questions
@@ -109,6 +159,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['questions'])
         self.assertTrue(data['total_questions'])
 
+    def test_search_questions_without_results(self):
+        """
+        Test search_questions endpoint to return no results
+        for search questions
+        """
+        url = '/questions/search'
+        res = self.client().post(url, json={
+            'searchTerm': 'eiffel'
+        })
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+
     def test_get_quiz(self):
         """
         Test get_quiz endpoint to return quiz data
@@ -123,6 +187,20 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['question'])
+
+    def test_get_quiz_with_no_category(self):
+        """
+        Test get_quiz endpoint to return quiz data
+        """
+        url = '/quizzes'
+        res = self.client().post(url, json={
+            'previous_questions': []
+        })
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
 
 
 # Make the tests conveniently executable
